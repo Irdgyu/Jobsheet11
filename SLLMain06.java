f8dcb1c8b9eabd05a93920ae793ea8b59a1dc6da
Irdgyu <gilangbayuirwana@gmail.com>
2025-06-01 15:02:04 +0700

    2.2

--- SLLMain06.java
+++ SLLMain06.java
@@ -1,31 +1,32 @@
-import java.util.Scanner;
-
 public class SLLMain06 {
     public static void main(String[] args) {
-        Scanner input6 = new Scanner(System.in);
         SingleLinkedList06 sll = new SingleLinkedList06();
 
-        System.out.print("Masukkan jumlah data mahasiswa: ");
-        int jumlah = input6.nextInt();
-        input6.nextLine();
-
-        for (int i = 0; i < jumlah; i++) {
-            System.out.println("Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM   : ");
-            String nim = input6.nextLine();
-            System.out.print("Nama  : ");
-            String nama = input6.nextLine();
-            System.out.print("Kelas : ");
-            String kelas = input6.nextLine();
-            System.out.print("IPK   : ");
-            double ipk = input6.nextDouble();
-            input6.nextLine();
-
-            Mahasiswa06 mhs = new Mahasiswa06(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-        }
-        System.out.println();
+        Mahasiswa06 mhs1 = new Mahasiswa06("24212200", "Alvaro", "1A", 4.0); 
+        Mahasiswa06 mhs2 = new Mahasiswa06("23212201", "Bimon", "2B", 3.8); 
+        Mahasiswa06 mhs3 = new Mahasiswa06("22212202", "Cintia", "3C", 3.5); 
+        Mahasiswa06 mhs4 = new Mahasiswa06("21212203", "Dirga", "4D", 3.6); 
+     
+        sll.print(); 
+        sll.addFirst(mhs4); 
+        sll.print(); 
+        sll.addLast(mhs1); 
+        sll.print(); 
+        sll.insertAfter("Dirga", mhs3);  
+        sll.insertAt(2, mhs2); 
         sll.print();
+        
+        System.out.println("data index 1 : "); 
+        sll.getData(1); 
+ 
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon")); 
+        System.out.println(); 
+ 
+        sll.removeFirst(); 
+        sll.removeLast(); 
+        sll.print(); 
+        sll.removeAt(0); 
+        sll.print(); 
     }
 }
 
